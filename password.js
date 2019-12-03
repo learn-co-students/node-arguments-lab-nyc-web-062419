67e2841f2f8ef61c106150c3bb2858a6f0a47272
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2019-12-03 21:04:07 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -1,6 +1,7 @@
 process.argv.shift()
 process.argv.shift()
 var input = process.argv
+var output = []
 var length = input[0] 
 var number = input[1]
 charset = charset || 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
