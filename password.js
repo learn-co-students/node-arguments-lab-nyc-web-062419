ba437ed0fb95edc94cfa9b92e5d72afd1bb95504
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2019-12-03 21:10:45 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -7,7 +7,7 @@ var number = parseInt(input[1])
 charset = charset || 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
 for (var i = number; i>0; i--){
-  for(var j = passLength; j>0; j--){
+  for(var j = passLength, n = charset.length; j>0; j--){
     var password = ""
     password += charset.charAt(Math.floor(Math.random() * n))
   }
