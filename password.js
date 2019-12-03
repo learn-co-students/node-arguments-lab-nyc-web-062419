41503b15e358077ee111c1c794df375eca10bbd6
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2019-12-03 21:06:19 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -6,7 +6,7 @@ var length = input[0]
 var number = input[1]
 charset = charset || 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-for (let i = number, i>0, i--){
+for (var i = number, i>0, i--){
   for(let j = length, j>0, j--){
     var password = ""
     password += charset.charAt(Math.floor(Math.random() * n))
