1f87ec9da59ef241ff5c995a3103b8b4de03b48e
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2019-12-17 04:36:01 +0000

    Done.

--- password.js
+++ password.js
@@ -1,30 +1,17 @@
-var generatePassword = function generatePassword(length, charset) {
-  var length = length || 8,
-    charset = charset || 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-    password = '';
-  for (var i = 0, n = charset.length; i < length; ++i) {
+var input = process.argv
+console.log(input)
+var output = []
+var passLength = parseInt(input[2], 10) 
+var number = parseInt(input[3], 10)
+charset = charset || 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+for (var i = number; i>0; i--){
+  for(var j = passLength, n = charset.length; j>0; j--){
+    var password = ""
     password += charset.charAt(Math.floor(Math.random() * n))
   }
-  return password
+  console.log(password)
 }
-
-var length = 10,
-  number = 1
-var argv = process.argv
-
-if (!argv[2] || !argv[3]) {
-  console.log('Please provide an argument, e.g., 5 or 5 10')
-  process.exit(1)
-}
-if (argv[2]) {
-  length = parseInt(argv[2], 10)
-}
-if (argv[3]) {
-  number = parseInt(argv[3], 10)
-}
-
-for (var i = 1; i <=number; i++) {
-  console.log(generatePassword(length))
-}
-
+process.stdout.write(output)
+console.log(output)
 process.exit(0)
